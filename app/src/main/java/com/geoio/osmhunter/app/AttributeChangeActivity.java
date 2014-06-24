7bdb05538a23afdf8a2298f1d5ff9e6a720dc01d
Leonard Techel <leonard@techel.net>
2014-06-24 16:30:58 +0200

    Add a simple adapter that shows text fields filled from the api

--- app/src/main/java/com/geoio/osmhunter/app/AttributeChangeActivity.java
+++ app/src/main/java/com/geoio/osmhunter/app/AttributeChangeActivity.java
@@ -2,17 +2,46 @@ package com.geoio.osmhunter.app;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AttributeChangeActivity extends Activity {
+
+    private ListView attributes_list_view;
+    List<JSONObject> attributes_list = new ArrayList<JSONObject>();
+    AttributeChangeAdapter attribute_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attribute_change);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+
+        attribute_adapter = new AttributeChangeAdapter(this, attributes_list);
+
+        attributes_list_view = (ListView) this.findViewById(R.id.attributes_list);
+        attributes_list_view.setAdapter(attribute_adapter);
+
+        updateAttributesList(id);
 
         final ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -40,6 +69,49 @@ public class AttributeChangeActivity extends Activity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void updateAttributesList(String id) {
+        Uri.Builder b = Uri.parse(getString(R.string.geoio_api_url)).buildUpon();
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        // build url
+        b.path("/buildings/");
+        b.appendPath(id);
+        b.appendPath("edit-form");
+        String url = b.build().toString();
+
+        // let's fix felix' pathes
+        if(!url.endsWith("/")) {
+            url += "/";
+        }
+
+        Log.v("x", url);
+
+        // fire http
+        client.get(url, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                // please do anything!
+            }
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    List<JSONObject> attributes_list_buffer = new ArrayList<JSONObject>();
+                    JSONArray result = response.getJSONArray("result");
+
+                    for(int i = 0; i < result.length(); i++) {
+                        attributes_list_buffer.add(result.getJSONObject(i));
+                    }
+
+                    attributes_list.addAll(attributes_list_buffer);
+                    attribute_adapter.notifyDataSetChanged();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
